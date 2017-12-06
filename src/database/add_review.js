c663be41017c3c3bb0cb7410d312d548d7f88687
Ghassan <ghassan.maslamani@gmail.com>
2017-12-06 10:55:39 +0200

    Adding new function between the database the backend
    Relates #10

---
+++ src/database/add_review.js
@@ -0,0 +1,20 @@
+const pl = require("./db_connection");
+const add_review = (user_id, place_id, review, rating, response) => {
+  pl.query(
+    "INSERT INTO reviews(user_id, place_id, review, rating) VALUES ($1, $2, $3, $4);",
+    [user_id, place_id, review, rating],
+    (err, res) => {
+      if (err) {
+        console.log(err);
+        console.log("error");
+        response.writeHead(500);
+        response.end();
+      } else {
+        console.log("success");
+        response.writeHead(200);
+        response.end("success");
+      }
+    }
+  );
+};
+module.exports = add_place;
