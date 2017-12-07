af134eb7b09a29fa996ffa79245cd2c5efceb570
nicholaspeal <nicholaspeal@gmail.com>
2017-12-07 14:20:36 +0200

    clean-up-xhr-and-readme REF #36' git push origin clean-up-xnr-and-readme git commit -m clean-up-xhr-and-readme

--- src/database/get_reviews.js
+++ src/database/get_reviews.js
@@ -1,21 +1,25 @@
-const pl = require("./db_connection");
+const pl = require('./db_connection');
 const get_reviews = (response, cafe) => {
   console.log('cafe in question', cafe);
-  pl.query("SELECT * FROM reviews WHERE place_id = (SELECT id FROM places WHERE name = $1)", [cafe], (err, res) => {
-    console.log('do we get here?');
-    if (err) {
-      console.log(err);
-      console.log("error");
-      response.writeHead(500);
-      response.end("DATABASE error");
-    } else {
-      console.log("success");
-      response.writeHead(200, {
-        "content-type": "application/json"
-      });
-      console.log('here is the review',JSON.stringify(res.rows));
-      response.end(JSON.stringify(res.rows));
+  pl.query(
+    'SELECT * FROM reviews WHERE place_id = (SELECT id FROM places WHERE name = $1)',
+    [cafe],
+    (err, res) => {
+      console.log('do we get here?');
+      if (err) {
+        console.log(err);
+        console.log('error');
+        response.writeHead(500);
+        response.end('DATABASE error');
+      } else {
+        console.log('success');
+        response.writeHead(200, {
+          'content-type': 'application/json'
+        });
+        console.log('here is the review', JSON.stringify(res.rows));
+        response.end(JSON.stringify(res.rows));
+      }
     }
-  });
+  );
 };
 module.exports = get_reviews;
