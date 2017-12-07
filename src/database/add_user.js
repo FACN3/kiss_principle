0c4e6ab113ac2637f946d8ff6c3b18b292b845c0
prodionov <pavelrodionov@hotmail.com>
2017-12-07 11:50:57 +0200

    we don't pass the response down to queries anymore Reference #38

--- src/database/add_user.js
+++ src/database/add_user.js
@@ -2,24 +2,14 @@
 //one . because it's in the same directory
 const pl = require("./db_connection");
 
-const add_user = (user_name, password, age, email, gender,response) => {
+const add_user = (user_name, password, age, email, gender,cb) => {
   console.log('user_name', user_name);
   console.log(`user name ${user_name}`);
   pl.query(
     'INSERT INTO users(user_name , password , age ,email , gender) VALUES ($1, $2, $3, $4, $5);',
     [user_name, password, age, email, gender],
     (err, res) => {
-      if (err) {
-        console.log(err);
-        console.log("error");
-        response.writeHead(500);
-        response.end();
-      } else {
-        console.log("success");
-        response.writeHead(200);
-        response.end("success");
-
-      }
+      cb(err);
     }
   );
 };
